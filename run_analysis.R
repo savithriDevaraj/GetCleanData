d11d9d4fdbcc1520c9b8d861f4ba6d155ce28f81
Savithri.Devaraj <sdevaraj@csc.com>
2015-02-16 17:04:55 -0500

    Updated to remove Step 5 from code

--- run_analysis.R
+++ run_analysis.R
@@ -26,6 +26,7 @@ dFeatures[, V2:=(gsub("(-|\\(|\\))|,","",V2))]
 #Read the acivity_labels file that contains the activity ID and the activity Name mapping
 dActivities <- data.table(read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE))
 
+#Step 2 - Combining the Subject/activity datasets and labelling the activities. Also label the measured variables.
 #Combine the train and test datasets to build the complete dataset
 dataX <- rbind(dTestX, dTrainX)
 dataY <-rbind(dTestY, dTrainY)
@@ -41,7 +42,6 @@ dataY$activity[dataY$V1 =="4"] <- as.character(dActivities[(V1=="4"),V2])
 dataY$activity[dataY$V1 =="5"] <- as.character(dActivities[(V1=="5"),V2])
 dataY$activity[dataY$V1 =="6"] <- as.character(dActivities[(V1=="6"),V2])
 
-
 #Column bind Subject and activity into another data table
 dataSubActivity <- cbind(dataSubj, dataY)
 
@@ -51,7 +51,7 @@ colnames(dataSubActivity) <- c("subject", "activityID","activity")
 #Name all variables in data_X using 2nd column in dfeatures
 colnames(dataX) <- as.character(dFeatures[,V2])
 
-
+#Step 3 - Choose only the required columns and build the clean dataset.
 #Select column names that have "std" or "mean" in the variables/features 
 matches <- dataX[,grep("(mean|std)", dFeatures$V2)]
 
@@ -64,32 +64,28 @@ dData <- cbind(dataSubActivity, limited)
 #Remove activityID column from data - Use activity label instead of activity ID makes it more understandable
 cleanData <- dData[,activityID:=NULL]
 
-# Step 4 is OVER! cleandata is the prepared data
+# Step 4 - Return the cleaned data for analysis
+cleanData
 
 # Step 5  
 # From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
 # Count number of groups by subject,activity
-#DT[,.N,by=V1]. This gives correct number = 180
+# The following gives correct number = 180
 
-cleanData[, .N, by=.(subject,activity)]
+#cleanData[, .N, by=.(subject,activity)]
 
-#DT[,.SD[c(1,.N)], by=V2]
-#cleandata[, .SD[3:.N], by=.(subject,activity)]
+# The following step gives the aggregated data by subject/activity.
+#tidyData <- cleanData[, lapply(.SD[3:.N], mean), by=.(subject,activity)]
 
-#DT[, lapply(.SD, sum), by=V2]
-#tidydata <- cleandata[, lapply(.SD[3:.N], mean), by=.(subject,activity)]
-tidyData <- cleanData[, lapply(.SD[3:.N], mean), by=.(subject,activity)]
+# Output the tidyData
+# tidyData
 
 # Order the data by subjects, so it is ready for analysis on subjects
-orderedTidyData <- tidyData[order(tidyData$subject),]
+# orderedTidyData <- tidyData[order(tidyData$subject),]
  
 #write.table() using row.name=FALSE
-write.table(orderedTidyData, "GetCleanDataFile", row.names=FALSE, col.names=TRUE, sep="\t",quote=FALSE)
+#write.table(orderedTidyData, "GetCleanDataFile", row.names=FALSE, col.names=TRUE, sep="\t",quote=FALSE)
 
-# Change the variable names to indicate they are the mean. Stuck here. Test how to set colnames.
-#colnames(tidydata)
-#I've using indexing. Basically using your activity column (the one with numbers) as indices to the #activity_labels.txt table and fetch the second column from that, and assign it back into your data.frame/data.table.
-#The simplistic way is to have six lines of subset (one line for each number) and use the subset to select the area of #an activityname variable to give a descriptive name. The most elegant is to turn the activity column into a factor, #then change the levels of the factor to text. If you have stuck all the data together it is also safe to use the week #three merge command.
 
